271b4851362b7498cf0e462cdfeeb0a6f523733a
Sathish Kumar Renganathan <sk015x@gmail.com>
2023-06-23 20:10:00 -0500

    basic changes

--- cdk_python_project/cdk_python_project_stack.py
+++ cdk_python_project/cdk_python_project_stack.py
@@ -1,16 +1,19 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
-from constructs import Construct
+from aws_cdk import core
+from aws_cdk import aws_s3 as _s3
 
-class CdkPythonProjectStack(Stack):
+class CdkPythonProjectStack(core.Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        _s3.Bucket(
+            self,
+            "myBucket",
+            bucket_name="cdkpythonprojectptack-sathish0101",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
 
         # example resource
         # queue = sqs.Queue(
